077e90ea7126b59ac60f307c6fbb45a46628f307
Daniel Beltejar <contacto@danielbeltejar.es>
2023-10-03 02:00:01 +0200

    Updated on 2023-10-03 02:00:01

--- bigmac-index-client.groovy
+++ bigmac-index-client.groovy
@@ -27,7 +27,7 @@ pipeline {
         projectFolder = "client"
         imageName = "${gitUrl.tokenize("/")[-1].replaceAll(".git", "")}-${projectFolder}"
         kind = "deployment"
-        kindName = "nginx-client-deployment"
+        kindName = "client-nginx-deployment"
         
         containerName = ""
         imageTag = ""
